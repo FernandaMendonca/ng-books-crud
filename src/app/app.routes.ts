aceba1ef8cb5835a3ae5b8ac7ed84add378f3b3b
Fernanda Mendonça <fernandasmendonca@outlook.com>
2024-10-01 22:35:00 -0300

    (new): add first routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { BooksListComponent } from '../components/books-list/books-list.component';
+import { AddBookComponent } from '../components/add-book/add-book.component';
+import { BookDetailComponent } from '../components/book-detail/book-detail.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },  // Redireciona para a lista de livros
+  { path: 'books', component: BooksListComponent },
+  { path: 'books/add', component: AddBookComponent },
+  { path: 'books/:id', component: BookDetailComponent }  // Página de detalhes do livro com ID dinâmico
+];
